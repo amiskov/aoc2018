c318cfa1c399b2a2829b680528e238d677143176
Andrey Miskov <amiskov@gmail.com>
2019-09-11 14:13:12 +0300

    Add part 2

--- day3/solution.rkt
+++ day3/solution.rkt
@@ -4,6 +4,8 @@
 (struct line (begin end) #:transparent)
 (struct claim (id lpad tpad end-x end-y) #:transparent)
 
+;; String->Claim
+;; Produce Claim from a given string
 (define (str->claim str)
   (match-define (list id lpad tpad width height)
     (map string->number
@@ -11,6 +13,8 @@
   (claim id lpad tpad (+ lpad width) (+ tpad height)))
 
 
+;; Line, Line -> Boolean
+;; Produce true if the beginning of the right line less than the end of left line
 (define (line-intersect? l1 l2)
   (match-define (list left right)
     (if (<= (line-begin l1) (line-begin l2))
@@ -26,7 +30,7 @@
                         (line (claim-tpad c2) (claim-end-y c2)))))
 
 
-;; Fill x coordinates for pointers in a row
+;; Fill x coordinates for points in a row
 (define (fill-xs x y end-x)
   (cond
     [(= x end-x) (set (point end-x y))]
@@ -57,8 +61,12 @@
   (if (not (claim-intersect? c1 c2))
       (set)
       (set-intersect (claim->points c1) (claim->points c2))))
-  
+
+
+;; Claim, ClaimList -> Set of Points
 (define (find-intersects-for-claim cl cl-list acc)
+  (display cl)
+  (display cl-list)
   (if (empty? cl-list)
       acc
       (find-intersects-for-claim cl
@@ -68,16 +76,47 @@
 
 
 (define (find-all-intersects data acc)
+  (display "\n")
   (if (empty? data) acc
       (let ([f (first data)]
             [r (rest data)])
-        (display (claim-id f)) (display " ")
+        ;(display (claim-id f)) (display " ")
         (find-all-intersects r
                              (find-intersects-for-claim f r acc)))))
 
 
 (define data (map str->claim (file->lines "input.txt")))
 
+
+;; Part 2
+
+;; Claim, List of Claims -> Boolean
+;; Returns true if claim has intersections
+(define (has-intersects cl data)
+  (if (empty? data)
+      false
+      (if (and
+           (not (= (claim-id cl) (claim-id (first data))))
+           (not (set-empty? (get-intersection cl (first data)))))
+          true
+          (has-intersects cl (rest data)))))
+
+
+;; List of Claims, List of Claims -> Claim ID
+;; Returns non overlapped claim ID
+(define (find-non-overlapped iter data)
+  (display ".")
+  (if (empty? iter) false
+      (let ([f (first iter)]
+            [r (rest iter)])
+        (if (not (has-intersects f data))
+            (claim-id f)
+            (find-non-overlapped r data)))))
+        
+
 (define start-time (current-seconds))
-(set-count (find-all-intersects data (set)))
-(- (current-seconds) start-time)
+(find-non-overlapped data data)
+;(set-count (find-all-intersects data (set)))
+(display (- (current-seconds) start-time)) (display "s")
+
+
